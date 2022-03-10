69dc004e2e96484fbabbf1db4ac6812d5219ade1
pavelchernov <p.chernov@city-mobil.ru>
2022-03-10 22:50:50 +0300

    добавил логи запроса

--- app/src/main/java/com/example/moviesearch/repository/RemoteDataSource.kt
+++ app/src/main/java/com/example/moviesearch/repository/RemoteDataSource.kt
@@ -3,9 +3,13 @@ package com.example.moviesearch.repository
 import com.example.moviesearch.BuildConfig
 import com.example.moviesearch.model.MovieDTO
 import com.google.gson.GsonBuilder
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Callback
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
 
 class RemoteDataSource {
     private val movieApi = Retrofit.Builder()
@@ -15,9 +19,26 @@ class RemoteDataSource {
                 GsonBuilder().setLenient().create()
             )
         )
+        .client(createOkHttpClient(MovieApiInterceptor()))
         .build().create(MovieAPI::class.java)
 
     fun getMovieDetails(id: Int, callback: Callback<MovieDTO>) {
         movieApi.getMovie(id, BuildConfig.MOVIE_API_KEY).enqueue(callback)
+    }
+
+    private fun createOkHttpClient(interceptor: Interceptor): OkHttpClient {
+        val httpClient = OkHttpClient.Builder()
+        httpClient.addInterceptor(interceptor)
+        httpClient.addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+
+        return httpClient.build()
+    }
+
+    inner class MovieApiInterceptor : Interceptor {
+
+        @Throws(IOException::class)
+        override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
+            return chain.proceed(chain.request())
+        }
     }
 }

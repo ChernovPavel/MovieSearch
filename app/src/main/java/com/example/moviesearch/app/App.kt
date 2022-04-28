acf257c0c177b58d5bc74d8b354e993ed69dae34
pavelchernov <pashcherno@yandex.ru>
2022-04-28 19:06:43 +0300

    fix бд

--- app/src/main/java/com/example/moviesearch/app/App.kt
+++ app/src/main/java/com/example/moviesearch/app/App.kt
@@ -6,30 +6,37 @@ import com.example.moviesearch.room.HistoryDao
 import com.example.moviesearch.room.HistoryDataBase
 
 class App : Application() {
+
     override fun onCreate() {
         super.onCreate()
         appInstance = this
+        createDBAsync()
     }
 
     companion object {
         private var appInstance: App? = null
-        private var db: HistoryDataBase? = null
+        private lateinit var db: HistoryDataBase
+
+        @Volatile
+        private var isDbInitialized = false
         private const val DB_NAME = "History.db"
 
         fun getHistoryDao(): HistoryDao {
+            return db.historyDao()
+        }
 
-            synchronized(HistoryDataBase::class.java) {
-                if (db == null) {
-                    if (appInstance == null) throw IllegalAccessException("APP must not be null")
-                    db = Room.databaseBuilder(
-                        appInstance!!.applicationContext,
-                        HistoryDataBase::class.java,
-                        DB_NAME
-                    )
-                        .build()
-                }
-            }
-            return db!!.historyDao()
+        private fun createDBAsync() {
+            if (isDbInitialized) return
+            Thread {
+                isDbInitialized = true
+                db = Room.databaseBuilder(
+                    appInstance!!.applicationContext,
+                    HistoryDataBase::class.java,
+                    DB_NAME
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+            }.start()
         }
     }
 }

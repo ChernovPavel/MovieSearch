c73e8c51196cedfd386ffdb1c76e532dd599c01b
pavelchernov <pashcherno@yandex.ru>
2022-05-24 13:28:00 +0300

    рефакторинг репозиториев

--- app/src/main/java/com/example/moviesearch/repository/DetailsRepository.kt
+++ app/src/main/java/com/example/moviesearch/repository/DetailsRepository.kt
@@ -1,7 +1,19 @@
 package com.example.moviesearch.repository
 
+import com.example.moviesearch.di.DetailsScope
 import com.example.moviesearch.model.MovieDTO
+import com.example.moviesearch.repository.api.RemoteDataSource
 import retrofit2.Response
+import javax.inject.Inject
+
+@DetailsScope
+class DetailsRepositoryImpl @Inject constructor(private val remoteDataSource: RemoteDataSource) :
+    DetailsRepository {
+
+    override suspend fun getMovieDetailsFromServer(id: Int): Response<MovieDTO> {
+        return remoteDataSource.getMovieDetails(id)
+    }
+}
 
 interface DetailsRepository {
     suspend fun getMovieDetailsFromServer(id: Int): Response<MovieDTO>
--- app/src/main/java/com/example/moviesearch/repository/LocalRepository.kt
+++ app/src/main/java/com/example/moviesearch/repository/LocalRepository.kt
@@ -1,6 +1,28 @@
 package com.example.moviesearch.repository
 
 import com.example.moviesearch.model.Movie
+import com.example.moviesearch.room.HistoryDao
+import com.example.moviesearch.room.NoteEntity
+import com.example.moviesearch.utils.convertHistoryEntityToMovie
+import com.example.moviesearch.utils.convertMovieToEntity
+
+class LocalRepositoryImpl(private val localDataSource: HistoryDao): LocalRepository {
+    override suspend fun getAllHistory(): List<Movie> {
+        return convertHistoryEntityToMovie(localDataSource.all())
+    }
+
+    override suspend fun saveEntity(movie: Movie) {
+        localDataSource.insert(convertMovieToEntity(movie))
+    }
+
+    override suspend fun saveNote(note: String, movieId: Int) {
+        localDataSource.insertNote(NoteEntity(movieId, note))
+    }
+
+    override suspend fun getNote(movieId: Int): String {
+        return localDataSource.getNote(movieId)
+    }
+}
 
 interface LocalRepository {
     suspend fun getAllHistory(): List<Movie>
